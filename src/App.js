5f6dc9de7b6db2b54cb46a8ca56ea8b91ec2f47c
kranti <krantiwaghmare21@navgurukul.org>
2023-10-30 20:23:26 +0530

    h1 tag add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header'
+import Addcart from "./Components/Addcart"
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Addcart/>
     </div>
   );
 }
